0881620980dc8f0afffd482e40ec0437a97ca182
邢中红 <2361767267@qq.com>
2026-03-02 12:17:34 +0800

    再次修改

--- Map.java
+++ Map.java
@@ -7,15 +7,11 @@ public class Map {
      private Point food;
      private SnakeBody snake;
      private Random random;
-     public Map()
+     public Map(SnakeBody snake)
      {
          this.snake=snake;
          this.random=new Random();
          generateFood();
-     }
-     public Map(SnakeBody snake)
-     {
-         this.snake=snake;
      }
      public void generateFood()
      {
@@ -57,26 +53,5 @@ public class Map {
      {
          return new Point(food.x, food.y);
      }
-     public boolean isEatingFood()
-     {
-         Point head=snake.getBody().get(0);
-         return head.equals(food);
-     }
-     public boolean isHittingWall()
-     {
-         Point head=snake.getBody().get(0);
-         return head.x<1||head.x>ROWS||head.y<1||head.y>COLS;
-     }
-     public boolean isHittingself()
-     {
-         Point head=snake.getBody().get(0);
-         for(int i=1;i<snake.getBody().size();i++)
-         {
-             if(head.equals(snake.getBody().get(i)))
-             {
-                 return true;
-             }
-         }
-         return false;
-     }
+
 }
